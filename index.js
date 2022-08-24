fb5ad7293dfea4c8b3b4ddac06e55cddac6fbe9d
Your Name <tpunchihewage@gmail.com>
2022-08-24 15:04:37 +0200

    added js file

---
+++ index.js
@@ -0,0 +1,44 @@
+const now = new Date();
+const test = new Date();
+
+// Question 01
+console.log(
+  "Q1",
+  '"now" is an object instantiated from the parent class "Date"'
+);
+
+// Question 02
+console.log("Q2", "Its an Object");
+
+// Question 03
+console.log("Q3", "its parent class");
+
+// Question 04
+console.log("Q4", "its an Object");
+
+// Question 05
+console.log("Q5", "current Date");
+
+// Question 06
+console.log("Q6", "current Date");
+
+// Question 07
+console.log("Q7", "current Date");
+
+// Question 08
+console.log("Q8", "no., its falsy value");
+
+// Question 09
+console.log("Q9", "False", "Because this two const are two different values");
+
+// Question 10
+console.log("Q10", "false");
+
+// Question 11
+console.log("Q11", "false");
+
+// Question 12
+console.log(
+  "Q12",
+  "Using instead a constructor as new Date(), each instance is unique (the two instances of the same constructor are still different to each-other), this is the reason why they are not equal when compared."
+);
